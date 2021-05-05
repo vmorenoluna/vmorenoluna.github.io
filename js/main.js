20118b67681700643dcc7dbaba466ddd29596319
vmorenoluna <morenolunav@gmail.com>
2021-05-05 23:39:43 +0100

    Added about button

--- js/main.js
+++ js/main.js
@@ -6,25 +6,49 @@ exclude: 'yes'
 
   $(document).ready(function () {
     {% if site.disable_landing_page != true %}
-    $('a.blog-button').click(function (e) {
-      if ($('.panel-cover').hasClass('panel-cover--collapsed')) return
-      currentWidth = $('.panel-cover').width()
-      if (currentWidth < 960) {
-        $('.panel-cover').addClass('panel-cover--collapsed')
-        $('.content-wrapper').addClass('animated slideInRight')
-      } else {
-        $('.panel-cover').css('max-width', currentWidth)
-        $('.panel-cover').animate({ 'max-width': '530px', 'width': '40%' }, 400, swing = 'swing', function () { })
+        $('a.blog-button').click(function (e) {
+            if ($('.panel-cover').hasClass('panel-cover--collapsed')) {
+                $('#root').load('/index.html #blog')
+                return
+            }
+            currentWidth = $('.panel-cover').width()
+            if (currentWidth < 960) {
+                $('.panel-cover').addClass('panel-cover--collapsed')
+                $('#root').load('/index.html #blog')
+            } else {
+                $('.panel-cover').css('max-width', currentWidth)
+                $('#root').load('/index.html #blog')
+                $('.panel-cover').animate({ 'max-width': '530px', 'width': '40%' }, 400, swing = 'swing', function () { })
+            }
+        })
+
+        if (window.location.hash && window.location.hash == '#blog') {
+            $('.panel-cover').addClass('panel-cover--collapsed')
+        }
+
+      $('a.about-button').click(function (e) {
+          if ($('.panel-cover').hasClass('panel-cover--collapsed')) {
+              $('#root').load('/index.html #about')
+              return
+          }
+          currentWidth = $('.panel-cover').width()
+          if (currentWidth < 960) {
+              $('.panel-cover').addClass('panel-cover--collapsed')
+              $('#root').load('/index.html #about')
+          } else {
+              $('.panel-cover').css('max-width', currentWidth)
+              $('#root').load('/index.html #about')
+              $('.panel-cover').animate({ 'max-width': '530px', 'width': '40%' }, 400, swing = 'swing', function () { })
+          }
+      })
+
+      if (window.location.hash && window.location.hash == '#about') {
+          $('.panel-cover').addClass('panel-cover--collapsed')
       }
-    })
 
-    if (window.location.hash && window.location.hash == '#blog') {
-      $('.panel-cover').addClass('panel-cover--collapsed')
-    }
-
-    if (window.location.pathname !== '{{ site.baseurl }}/' && window.location.pathname !== '{{ site.baseurl }}/index.html') {
-      $('.panel-cover').addClass('panel-cover--collapsed')
-    }
+        if (window.location.pathname !== '{{ site.baseurl }}/' && window.location.pathname !== '{{ site.baseurl }}/index.html') {
+            $('.panel-cover').addClass('panel-cover--collapsed')
+        }
     {% endif %}
 
     $('.btn-mobile-menu').click(function () {
@@ -35,6 +59,11 @@ exclude: 'yes'
     $('.navigation-wrapper .blog-button').click(function () {
       $('.navigation-wrapper').toggleClass('visible')
       $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
+    })
+
+    $('.navigation-wrapper .cover-button').click(function () {
+        $('.navigation-wrapper').toggleClass('visible')
+        $('.btn-mobile-menu__icon').toggleClass('icon-list icon-x-circle animated fadeIn')
     })
 
   })
